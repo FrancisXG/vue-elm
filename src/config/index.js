1b0e3a97f0063d7e3daa9757887cfe5e3a71129a
Francis <41093513+FrancisXG@users.noreply.github.com>
2020-08-06 12:04:23 +0100

    target proxy

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
     title: "elm",
     baseUrl: {
-        dev: "http://localhost:3000",//开发的时候后台接口的地址
+        dev: "/api/",//开发的时候后台接口的地址
         pro: "" //上线产品发布之后，后台接口的地址
     }
 }
